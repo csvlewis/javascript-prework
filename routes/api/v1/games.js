30407b92374b371dd4687ef09f2a1b00b5d0f533
Chris Lewis <13chlewis@gmail.com>
2019-04-27 10:15:08 -0600

    Created remaining routes

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 var Game = require('../../../models').Game;
 
-/* GET all games */
 router.get("/", function(req, res, next) {
   Game.findAll()
     .then(games => {
@@ -30,5 +29,62 @@ router.get("/:id", function(req, res, next) {
       res.status(500).send({error})
     });
 });
+
+router.post("/", function(req, res, next) {
+  Game.create({
+          title: req.body.title,
+          price: req.body.price,
+          releaseYear: req.body.releaseYear,
+          active: req.body.active
+    })
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(201).send(JSON.stringify(game));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error });
+    });
+});
+
+router.put("/:id", function(req, res, next) {
+  Game.update({
+          title: req.body.title,
+          price: req.body.price,
+          releaseYear: req.body.releaseYear,
+          active: req.body.active
+    }),
+    {
+      returning: true,
+      where: {
+        id: parseInt(req.params.id)
+      }
+    }
+    .then(([rowsUpdate, [updatedGame]]) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(202).send(JSON.stringify(updatedGame));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error });
+    });
+});
+
+router.delete("/:id", function(req, res, next) {
+  Game.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(204);
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({error})
+    });
+});
+
 
 module.exports = router; //this should stay at the bottom of the file
